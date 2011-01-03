b45da6e3a4b6e381e339810ba5f7b3408847b406
Jean-Denis Vauguet <jd@vauguet.fr>
2011-01-03 18:53:21 +0100

    some more feature and examples

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,6 @@
 #require File.expand_path("../../../lib/logg.rb",  __FILE__)
+#path = File.expand_path(File.dirname(__FILE__) + '/../../lib/logg.rb')
+#ENV['PATH'] = ENV['PATH'] + ":#{path}"
 require 'aruba/cucumber'
 
 Before do
