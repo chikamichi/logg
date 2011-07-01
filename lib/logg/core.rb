444658d3f00ae45da34820889afec46074be40d9
Jean-Denis Vauguet <jd@vauguet.fr>
2011-07-01 17:44:51 +0200

    some doc on the core lib

--- lib/logg/core.rb
+++ lib/logg/core.rb
@@ -1,5 +1,8 @@
 module Logg
-  class Machine 
+  # A Machine is a logger core implementation, providing logger's definition
+  # and output methods. It's not intented to be used directly but through the
+  # Er mixin, within a class.
+  class Machine
     attr_reader :message, :namespace
 
     # The Machine is based on #method_missing. It auto-sets both the message
@@ -20,14 +23,15 @@ module Logg
       # TODO: respond_to? if/else
       method = method.to_sym
 
+      # Define the guard at class-level if not already defined.
       if !eigenclass.respond_to?(method)
-        # Define the guard at class-level
         eigenclass.send(:define_method, method) do |*args|
           yield *args
         end
       end
 
-      # Define the guard at instance-level
+      # Define the guard at instance-level by overriding #initialize, if not
+      # already defined.
       eigenclass.send(:define_method, :new) do
         o = super
         if !o.respond_to?(method)
@@ -43,28 +47,37 @@ module Logg
 
     # TODO: handle the output with a templating system
     def output!
-      debug  = "#{Time.now} | "
-      debug += "[#{@namespace}] " unless @namespace.nil?
-      debug += @message
-      puts debug
-      return debug
+      output  = "#{Time.now} | "
+      output += "[#{@namespace}] " unless @namespace.nil?
+      output += @message
+      puts output
+      return output
     end
   end
 
+  # The Er module, when mixed-in a class, instantiates a Machine and performs some
+  # simple meta-programming on this receiver to add support for the logger. It thus
+  # enable the receiver to use the logger's default implementation and/or define
+  # custom loggers.
   module Er
     LOGGER = Logg::Machine.new
 
     def self.included(base)
       if !base.respond_to?(:logger)
         base.instance_eval do
+          # Memoized logger for the receiver's class.
+          #
+          # TODO: add support for defining the logger under a different name than #logger
+          # this means either defining a constant before mixin, or delaying the metaprog.
           def self.logger
             @@_logg_er ||= LOGGER
           end
         end
       else
-        raise RuntimeError, "Cannot mixin Logg::Er as #{base}#logger, already defined."
+        raise RuntimeError, "Cannot mixin Logg::Er as #{base}#logger, method's already defined."
       end
 
+      # Memoized logger for the receiver.
       base.extend RedefInit
     end
 
